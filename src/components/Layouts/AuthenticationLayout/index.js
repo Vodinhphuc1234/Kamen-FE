a920f5d95f12dd0b2b76593f6ade3743d267754d
vodinhphuc1234 <phucatoniegriezman@gmail.com>
2023-04-15 16:25:06 +0700

    Update authentacation layout

--- src/components/Layouts/AuthenticationLayout/index.js
+++ src/components/Layouts/AuthenticationLayout/index.js
@@ -1,12 +1,17 @@
+import { Col, Row } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 
 export default function AuthenticationLayout() {
   return (
-    <div className="h-100 w-100 d-flex">
-      <img className="w-50" src="img/login-bg.jpg" alt="none" />
-      <div className="w-50">
-        <Outlet />
-      </div>
-    </div>
+    <Row className="h-100 w-100">
+      <Col>
+        <img className="w-100" src="img/login-bg.jpg" alt="none" />
+      </Col>
+      <Col>
+        <div className="w-100">
+          <Outlet />
+        </div>
+      </Col>
+    </Row>
   );
 }
